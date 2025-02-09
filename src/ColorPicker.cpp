16ce6127d678b9c1adb1781aa86a0f3c497c1e78
Benjamin <BECOD1@ulaval.ca>
2025-02-09 15:55:43 -0500

    HSB sliders

--- src/ColorPicker.cpp
+++ src/ColorPicker.cpp
@@ -1,4 +1,5 @@
 #include "ColorPicker.h"
+#include <imgui_internal.h>
 
 void ColorPicker::setup() 
 {
@@ -14,9 +15,17 @@ void ColorPicker::draw()
 	{
 		ImGui::Begin("ColorPicker", &showColorPicker, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize);
 
-		if (ImGui::Button("RGB")) colorPickerMode = ColorPickerMode::RGB;
+		if (ImGui::Button("RGB")) 
+			colorPickerMode = ColorPickerMode::RGB;
 		ImGui::SameLine();
-		if (ImGui::Button("HSB")) colorPickerMode = ColorPickerMode::HSB;
+		if (ImGui::Button("HSB")) 
+		{
+			colorPickerMode = ColorPickerMode::HSB;
+			HSB hsb = RGBToHSB(selectedColor[0], selectedColor[1], selectedColor[2]);
+			hueSlider = hsb.h;
+			saturationSlider = hsb.s;
+			brightnessSlider = hsb.b;
+		} 
 
 		switch (colorPickerMode) 
 		{
@@ -26,13 +35,134 @@ void ColorPicker::draw()
 			ImGui::ColorPicker4("RGB", selectedColor, ImGuiColorEditFlags_DisplayRGB);
 			break;
 		case ColorPickerMode::HSB:
-			ImGui::ColorPicker4("HSB", selectedColor, ImGuiColorEditFlags_DisplayHSV);
+			float prevHue = hueSlider, prevSaturation = saturationSlider, prevBrightness = brightnessSlider;
+
+			HSB saturationHSBNone, saturationHSBFull = hsb;
+			saturationHSBNone.s = 0.0f;
+			saturationHSBFull.s = 1.0f;
+			RGB saturationRGBStart = HSBToRGB(saturationHSBNone.h, saturationHSBNone.s, saturationHSBNone.b);
+			RGB saturationRGBEnd = HSBToRGB(saturationHSBFull.h, saturationHSBFull.s, saturationHSBFull.b);
+
+			ImU32 saturationColorStart = ImGui::GetColorU32(ImVec4(saturationRGBStart.r, saturationRGBStart.g, saturationRGBStart.b, 1.0f));
+			ImU32 saturationColorEnd = ImGui::GetColorU32(ImVec4(saturationRGBEnd.r, saturationRGBEnd.g, saturationRGBEnd.b, 1.0f));
+
+			ImU32 brightnessColorStart = ImGui::GetColorU32(ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
+			ImU32 brightnessColorEnd = ImGui::GetColorU32(ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+			ImU32 alphaColorStart = ImGui::GetColorU32(ImVec4(selectedColor[0], selectedColor[1], selectedColor[2], 0.0f));
+			ImU32 alphaColorEnd = ImGui::GetColorU32(ImVec4(selectedColor[0], selectedColor[1], selectedColor[2], 1.0f));
+
+			HueSlider("Hue", &hueSlider, 0.0f, 360.0f);
+			GradientSlider("Saturation", &saturationSlider, 0.0f, 1.0f, saturationColorStart, saturationColorEnd);
+			GradientSlider("Brightness", &brightnessSlider, 0.0f, 1.0f, brightnessColorStart, brightnessColorEnd);
+			GradientSlider("Alpha", &selectedColor[3], 0.0f, 1.0f, alphaColorStart, alphaColorEnd);
+
+			if (prevHue != hueSlider || prevSaturation != saturationSlider || prevBrightness != brightnessSlider) 
+			{
+				rgb = HSBToRGB(hueSlider, saturationSlider, brightnessSlider);
+				hsb = { hueSlider, saturationSlider, brightnessSlider };
+				selectedColor[0] = rgb.r, selectedColor[1] = rgb.g, selectedColor[2] = rgb.b;
+			}
 
 			break;
 		}
 
 		ImGui::End();
 	}
+}
+
+void ColorPicker::HueSlider(const char* label, float* value, float valueMin, float valueMax) 
+{
+	ImGui::PushID(label);
+	ImVec2 p = ImGui::GetCursorScreenPos();
+	float width = ImGui::CalcItemWidth();
+	float height = ImGui::GetFrameHeight();
+	float margin = 1.5f;
+	float grabWidth = height - margin;
+	ImRect grabBoundingBox = ImRect(ImVec2(p.x + height * 0.5f, p.y), ImVec2(p.x + width - height * 0.5f, p.y + height));
+
+	ImGui::InvisibleButton(label, ImVec2(width, height));
+	bool isActive = ImGui::IsItemActive();
+	bool clicked = ImGui::IsItemClicked();
+
+	if (isActive)
+	{
+		float mousePos = ImGui::GetMousePos().x - grabBoundingBox.Min.x;
+		*value = valueMin + (valueMax - valueMin) * (mousePos / grabBoundingBox.GetWidth());
+		*value = ImClamp(*value, valueMin, valueMax);
+	}
+
+	//Draw background
+	ImDrawList* drawList = ImGui::GetWindowDrawList();
+	float sixth = width / 6.0f;
+
+	drawList->AddRectFilledMultiColor(ImVec2(p.x, p.y), ImVec2(p.x + sixth, p.y + height), red, yellow, yellow, red);
+	drawList->AddRectFilledMultiColor(ImVec2(p.x + sixth, p.y), ImVec2(p.x + sixth * 2, p.y + height), yellow, green, green, yellow);
+	drawList->AddRectFilledMultiColor(ImVec2(p.x + sixth * 2, p.y), ImVec2(p.x + sixth * 3, p.y + height), green, cyan, cyan, green);
+	drawList->AddRectFilledMultiColor(ImVec2(p.x + sixth * 3, p.y), ImVec2(p.x + sixth * 4, p.y + height), cyan, blue, blue, cyan);
+	drawList->AddRectFilledMultiColor(ImVec2(p.x + sixth * 4, p.y), ImVec2(p.x + sixth * 5, p.y + height), blue, magenta, magenta, blue);
+	drawList->AddRectFilledMultiColor(ImVec2(p.x + sixth * 5, p.y), ImVec2(p.x + sixth * 6, p.y + height), magenta, red, red, magenta);
+
+	//Draw grab
+	float grabPos = (*value - valueMin) / (valueMax - valueMin) * grabBoundingBox.GetWidth();
+	float grabX = p.x + grabPos;
+	drawList->AddRectFilled(ImVec2(grabX + margin, p.y + grabWidth), ImVec2(grabX + grabWidth, p.y + margin), ImGui::GetColorU32(ImVec4(0.0f, 0.0f, 0.0f, 1.0f)));
+	drawList->AddRectFilled(ImVec2(grabX + margin * 1.5f, p.y + grabWidth - margin * 0.5f), ImVec2(grabX + grabWidth - margin * 0.5f, p.y + margin * 1.5f), ImGui::GetColorU32(isActive ? ImGuiCol_SliderGrabActive : ImGuiCol_SliderGrab));
+
+	//Draw value
+	ImVec2 textSize = ImGui::CalcTextSize(label);
+	ImVec2 textPos = ImVec2(p.x + (width - textSize.x) * 0.5f, p.y + (height - textSize.y) * 0.5f);
+	char cStringValue[50];
+	sprintf(cStringValue, "%.3f", *value);
+	drawList->AddText(textPos, ImGui::GetColorU32(ImVec4(1.0f, 1.0f, 1.0f, 1.0f)), cStringValue);
+
+	ImGui::SameLine();
+	ImGui::Text(label);
+
+	ImGui::PopID();
+}
+
+void ColorPicker::GradientSlider(const char* label, float* value, float valueMin, float valueMax, ImU32 colorStart, ImU32 colorEnd) 
+{
+	ImGui::PushID(label);
+	ImVec2 p = ImGui::GetCursorScreenPos();
+	float width = ImGui::CalcItemWidth();
+	float height = ImGui::GetFrameHeight();
+	float margin = 1.5f;
+	float grabWidth = height - margin;
+	ImRect grabBoundingBox = ImRect(ImVec2(p.x + height * 0.5f, p.y), ImVec2(p.x + width - height * 0.5f, p.y + height));
+
+	ImGui::InvisibleButton(label, ImVec2(width, height));
+	bool isActive = ImGui::IsItemActive();
+	bool clicked = ImGui::IsItemClicked();
+
+	if (isActive) 
+	{
+		float mousePos = ImGui::GetMousePos().x - grabBoundingBox.Min.x;
+		*value = valueMin + (valueMax - valueMin) * (mousePos / grabBoundingBox.GetWidth());
+		*value = ImClamp(*value, valueMin, valueMax);
+	}
+
+	//Draw background
+	ImDrawList* drawList = ImGui::GetWindowDrawList();
+	drawList->AddRectFilledMultiColor(p, ImVec2(p.x + width, p.y + height), colorStart, colorEnd, colorEnd, colorStart);
+
+	//Draw grab
+	float grabPos = (*value - valueMin) / (valueMax - valueMin) * grabBoundingBox.GetWidth();
+	float grabX = p.x + grabPos;
+	drawList->AddRectFilled(ImVec2(grabX + margin, p.y + grabWidth), ImVec2(grabX + grabWidth, p.y + margin), ImGui::GetColorU32(ImVec4(0.0f,0.0f,0.0f,1.0f)));
+	drawList->AddRectFilled(ImVec2(grabX + margin * 1.5f, p.y + grabWidth - margin * 0.5f), ImVec2(grabX + grabWidth - margin * 0.5f, p.y + margin * 1.5f), ImGui::GetColorU32(isActive ? ImGuiCol_SliderGrabActive : ImGuiCol_SliderGrab));
+
+	//Draw value
+	ImVec2 textSize = ImGui::CalcTextSize(label);
+	ImVec2 textPos = ImVec2(p.x + (width - textSize.x) * 0.5f, p.y + (height - textSize.y) * 0.5f);
+	char cStringValue[50];
+	sprintf(cStringValue, "%.3f", *value);
+	drawList->AddText(textPos, ImGui::GetColorU32(ImVec4(1.0f, 1.0f, 1.0f, 1.0f)), cStringValue);
+
+	ImGui::SameLine();
+	ImGui::Text(label);
+
+	ImGui::PopID();
 }
 
 ColorPicker::HSB ColorPicker::RGBToHSB(float r, float g, float b) 
--- src/ColorPicker.h
+++ src/ColorPicker.h
@@ -8,49 +8,67 @@ class ColorPicker
 		void setup();
 		void draw();
 
-		bool showColorPicker;
-		float selectedColor[4];
-
-	private:
-		struct HSB 
+		struct HSB
 		{
 			float h;
 			float s;
 			float b;
 		};
 
-		struct RGB 
+		struct RGB
 		{
 			float r;
 			float g;
 			float b;
 		};
 
+		bool showColorPicker;
+		float selectedColor[4];
+		HSB hsb;
+		RGB rgb;
+private:
+		enum class ColorPickerMode { RGB, HSB };
+
+		ColorPickerMode colorPickerMode;
+
+		float hueSlider;
+		float saturationSlider;
+		float brightnessSlider;
+
+		void GradientSlider(const char* label, float* value, float valueMin, float valueMax, ImU32 colorStart, ImU32 colorEnd);
+		void HueSlider(const char* label, float* value, float valueMin, float valueMax);
+		ImU32 red = ImGui::GetColorU32(ImVec4(255, 0, 0, 1));
+		ImU32 yellow = ImGui::GetColorU32(ImVec4(255, 255, 0, 1));
+		ImU32 green = ImGui::GetColorU32(ImVec4(0, 255, 0, 1));
+		ImU32 cyan = ImGui::GetColorU32(ImVec4(0, 255, 255, 1));
+		ImU32 blue = ImGui::GetColorU32(ImVec4(0, 0, 255, 1));
+		ImU32 magenta = ImGui::GetColorU32(ImVec4(255, 0, 255, 1));
+
 		/**
 		* \brief Transforme un rgb en hsb (Hue, Saturation, Brightness)
+		* Possiblement extraire en methode Utils si besoin ailleurs
+		*
 		* \param float r(0, 1)
 		* \param float g(0, 1)
 		* \param float b(0, 1)
-		* 
-		* \return float hue(0, 360) 
-		* \return float saturation(0, 1) 
+		*
+		* \return float hue(0, 360)
+		* \return float saturation(0, 1)
 		* \return float brightness(0, 1)
 		*/
 		HSB RGBToHSB(float r, float g, float b);
 
 		/**
 		* \brief Transforme un hsb (Hue, Saturation, Brightness) en rgb
+		* Possiblement extraire en methode Utils si besoin ailleurs
+		*
 		* \param float h(0, 360)
 		* \param float s(0, 1)
 		* \param float b(0, 1)
-		* 
+		*
 		* \return float r(0, 1)
 		* \return float g(0, 1)
 		* \return float b(0, 1)
 		*/
 		RGB HSBToRGB(float h, float s, float b);
-
-		enum class ColorPickerMode { RGB, HSB };
-
-		ColorPickerMode colorPickerMode;
 };

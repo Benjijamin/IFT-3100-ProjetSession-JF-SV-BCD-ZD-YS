84af1ad76a15c85f24d1c8f3c74e69a9021de1b6
Yohan Sercia <yohansercia@outlook.com>
2025-02-17 16:50:36 -0500

    1. Ajout de plusieurs menu déroulant depuis le template MenuBar. 2. Implémentation de:     -Open Project     -Save Project     -Import/Export (la méthode est correctement appelée depuis la classe ImageEditor, mais l'image n'affiche pas. Est-ce cette fonctionnalitée est plustôt prise en charge dans AssetBrowser ?     -Exit de l'application

--- src/MenuBar.cpp
+++ src/MenuBar.cpp
@@ -2,14 +2,114 @@
 
 void MenuBar::drawGui() 
 {
-	if (ImGui::BeginMainMenuBar()) 
-	{
-		if (ImGui::BeginMenu("File")) 
-		{
-			if (ImGui::MenuItem("Settings")) 
-			{
-				//Ouvrir settings
+	if (ImGui::BeginMainMenuBar()) {
+		/*
+		* ONGLET DES FICHIERS
+		* 
+		* ---Contenu---
+		* >Nouveau projet
+		* >Ouvrir un projet sauvegardé ou existant (bin/data)
+		* >Sauvegarder un projet dans un fichier (bin/data)
+		* >Import/Export (pour image ou autre)
+		* >Paramètres de l'interface (si besoin)
+		* >Quitter l'interface
+		*/
+		if (ImGui::BeginMenu("File")) {
+
+			if (ImGui::MenuItem("New Project")) {}
+
+			if (ImGui::MenuItem("Open Project")) {
+				ofFileDialogResult result = ofSystemLoadDialog("Choisissez un fichier de projet");
+				if (result.bSuccess) {
+					openProject(result.getPath());
+				}
+
 			}
+			if (ImGui::MenuItem("Save Project")) {
+				ofFileDialogResult result = ofSystemSaveDialog("monProjet.json","Enregistrer sous ...");
+				if (result.bSuccess) {
+					saveProject((result.getPath()));
+				}
+				
+				
+			}
+			
+			if (ImGui::MenuItem("Import")) {
+				ofFileDialogResult result = ofSystemLoadDialog("Choisissez une image à charger");
+				if (result.bSuccess) {
+					imageEditor.load(result.getPath());
+				}
+			}
+
+			if (ImGui::MenuItem("Export")) {
+				ofFileDialogResult result = ofSystemSaveDialog("monImage.png", "Enregistrer sous ...");
+				if (result.bSuccess) {
+					imageEditor.save(result.getPath());
+				}
+			}
+
+
+
+			ImGui::Separator();
+
+			if (ImGui::MenuItem("Settings")) {}
+			if (ImGui::MenuItem("Exit")) {
+			ofExit();
+			}
+
+			ImGui::EndMenu();
+		}
+
+		/*
+		* ONGLET DE L'ÉDITEUR
+		* 
+		* ---Contenu---
+		* >Undo/redo/delete/etc.
+		* >Déplacement des primitives
+		* >Modification des primitives
+		* >Autres
+		*/
+		if (ImGui::BeginMenu("Edit")) {
+			if (ImGui::MenuItem("Undo", "Ctrl+Z")) {}
+			if (ImGui::MenuItem("Redo", "Ctrl+Y")) {}
+
+			ImGui::Separator();
+
+			if (ImGui::MenuItem("Cut", "Ctrl+X")) {}
+			if (ImGui::MenuItem("Copy", "Ctrl+C")) {}
+			if (ImGui::MenuItem("Paste", "Ctrl+V")) {}
+
+			ImGui::EndMenu();
+		}
+
+		/*
+		* ONGLET DU RENDEUR D'IMAGE
+		*
+		* ---Contenu---
+		* >
+		*/
+		if (ImGui::BeginMenu("Render")) {
+			if (ImGui::MenuItem("Render Image")) {}
+			if (ImGui::MenuItem("Render Animation")) {}
+
+			ImGui::EndMenu();
+		}
+
+		/*
+		* ONGLET DE FENÊTRE
+		*
+		* ---Contenu---
+		* >Plein écran
+		* >Séparer le canva en deux
+		* >Rétablir le layout original
+		*/
+		if (ImGui::BeginMenu("Window")) {
+			if (ImGui::MenuItem("Toggle Fullscreen")) {}
+			if (ImGui::MenuItem("Split canva")) {}
+
+			ImGui::Separator();
+
+			if (ImGui::MenuItem("Reset Layout")) {}
 
 			ImGui::EndMenu();
 		}
@@ -17,3 +117,20 @@ void MenuBar::drawGui()
 		ImGui::EndMainMenuBar();
 	}
 }
+
+void MenuBar::saveProject(const std::string& filePath) {
+	ofFile file(filePath);
+	if (!file.exists()) {
+		ofLogError() << "Fichier non trouvé : " << filePath;
+		return;
+	}
+}
+
+void MenuBar::openProject(const std::string& filePath) {
+	ofFile file(filePath);
+	if (!file.exists()) {
+		ofLogError() << "Fichier non trouvé : " << filePath;
+		return;
+	}
+}
+
--- src/MenuBar.h
+++ src/MenuBar.h
@@ -1,9 +1,34 @@
 #pragma once
 
 #include "ofxImGui.h"
+#include "ofMain.h"
+#include "imageEditor.h"
 
+/*
+* \brief Classe qui gère la barre de menu.
+*/
 class MenuBar 
 {
 	public:
+		/*
+		* \brief Méthode de la librairie openframeworks
+		*/
 		void drawGui();
+
+		/*
+		* \brief Gestion de la sauvegarde des projets
+		* \param filePath --> Type .json
+		*/
+		void saveProject(const std::string& filePath);
+
+		/*
+		* \brief Gestion du chargement de projets existants
+		* \param filePath --> Type .json
+		*/
+		void openProject(const std::string& filePath);
+
+		/*
+		* \brief Instance de 'ImageEditor' pour accéder à ses méthodes
+		*/
+		ImageEditor imageEditor;
 };

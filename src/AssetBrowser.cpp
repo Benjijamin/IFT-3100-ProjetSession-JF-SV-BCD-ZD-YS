20acde1f8cfb723ac9ab980866dfd1dc37894696
Zachary Duval <zachary.duval.1@ulaval.ca>
2025-02-20 10:41:34 -0500

    Improved AssetBrowser

--- src/AssetBrowser.cpp
+++ src/AssetBrowser.cpp
@@ -17,57 +17,123 @@ void AssetBrowser::draw() {
 }
 
 void AssetBrowser::drawGui() {
-    ImGui::SetNextWindowSizeConstraints(ImVec2(400, 200), ImVec2(FLT_MAX, FLT_MAX));
+    ImGuiIO& io = ImGui::GetIO();
+    ImVec2 windowSize = io.DisplaySize;
 
-    ImGui::Begin("Asset Browser");
+    static float assetBrowserHeight = 300.0f;
+    static bool resizing = false;
 
-    ImVec2 contentRegion = ImGui::GetContentRegionAvail();
-    float scrollableHeight = contentRegion.y - 100;
+    ImGui::SetNextWindowPos(ImVec2(0, windowSize.y - assetBrowserHeight));
+    ImGui::SetNextWindowSize(ImVec2(windowSize.x, assetBrowserHeight));
 
-    ImGui::BeginChild("AssetList", ImVec2(0, scrollableHeight), true);
+    ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove;
 
-    auto filteredAssets = getFilteredAssets();
-    for (const auto& asset : filteredAssets) {
-        std::string displayText = showFullPaths ? asset : fs::path(asset).filename().string();
-        if (ImGui::Selectable(displayText.c_str(), selectedAsset == asset)) {
-            selectedAsset = asset;
-            if (onAssetSelection) onAssetSelection();
+    if (ImGui::Begin("Asset Browser", nullptr, windowFlags)) {
+        if (ImGui::IsWindowHovered(ImGuiHoveredFlags_AllowWhenBlockedByActiveItem)) {
+            if (ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
+                resizing = true;
+            }
         }
-    }
 
-    ImGui::EndChild();
+        if (resizing && ImGui::IsMouseDragging(ImGuiMouseButton_Left)) {
+            float mouseDelta = io.MouseDelta.y;
+            assetBrowserHeight -= mouseDelta;
 
-    float windowPaddingY = ImGui::GetStyle().WindowPadding.y;
-    ImGui::Dummy(ImVec2(0.0f, windowPaddingY));
-
-    if (!selectedAsset.empty()) {
-        if (ImGui::Button("Delete")) {
-            removeAsset(selectedAsset);
-            if (onAssetDeletion) onAssetDeletion();
-            selectedAsset.clear();
+            if (assetBrowserHeight < 100.0f) {
+                assetBrowserHeight = 100.0f;
+            }
+            if (assetBrowserHeight > windowSize.y - 100.0f) {
+                assetBrowserHeight = windowSize.y - 100.0f;
+            }
         }
-    }
-
-    if (ImGui::Button("Load New Asset")) {
-        ofFileDialogResult result = ofSystemLoadDialog("Select an asset");
-        if (result.bSuccess) {
-            addAsset(result.getPath());
+        else if (resizing && !ImGui::IsMouseDown(ImGuiMouseButton_Left)) {
+            resizing = false;
         }
-    }
 
-    if (ImGui::Button("Load Assets from Folder")) {
-        ofFileDialogResult result = ofSystemLoadDialog("Select a folder", true);
-        if (result.bSuccess) {
-            loadAssetsFromFolder(result.getPath());
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(40, 40, 40, 255));
+        ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(30, 30, 30, 255));
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(10, 10));
+        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(10, 10));
+
+        static char searchBuffer[128] = "";
+        ImGui::InputTextWithHint("##Search", "Search...", searchBuffer, IM_ARRAYSIZE(searchBuffer));
+        ImGui::Dummy(ImVec2(0.0f, 4.0f));
+
+        ImVec2 contentRegion = ImGui::GetContentRegionAvail();
+        float availableHeight = contentRegion.y;
+
+        ImGui::BeginChild("AssetList", ImVec2(contentRegion.x * 0.75f, availableHeight), false);
+
+        auto filteredAssets = getFilteredAssets();
+
+        for (const auto& asset : filteredAssets) {
+            std::string displayText = showFullPaths ? asset : fs::path(asset).stem().string();
+
+            if (strstr(displayText.c_str(), searchBuffer) != nullptr) {
+                ImVec2 selectableSize = ImVec2(0, ImGui::GetFrameHeightWithSpacing());
+                ImGui::BeginGroup();
+
+                if (ImGui::Selectable("##hidden", selectedAsset == asset, 0, selectableSize)) {
+                    selectedAsset = asset;
+                    if (onAssetSelection) onAssetSelection();
+                }
+
+                ImGui::SameLine();
+                ImVec2 textPos = ImGui::GetCursorPos();
+                textPos.y += (selectableSize.y - ImGui::GetTextLineHeight()) / 2;
+                ImGui::SetCursorPos(textPos);
+                ImGui::Text(displayText.c_str());
+
+                if (!showFullPaths) {
+                    ImGui::SameLine(contentRegion.x * 0.5f);
+                    ImGui::SetCursorPosY(textPos.y);
+                    ImGui::TextDisabled("[%s]", fs::path(asset).extension().string().c_str());
+                }
+
+                ImGui::EndGroup();
+            }
         }
+
+        ImGui::EndChild();
+
+        ImGui::SameLine();
+
+        ImGui::BeginChild("Controls", ImVec2(contentRegion.x * 0.25f, availableHeight), false);
+
+        if (!selectedAsset.empty()) {
+            if (ImGui::Button("Delete")) {
+                removeAsset(selectedAsset);
+                if (onAssetDeletion) onAssetDeletion();
+                selectedAsset.clear();
+            }
+        }
+
+        if (ImGui::Button("Load New Asset")) {
+            ofFileDialogResult result = ofSystemLoadDialog("Select an asset");
+            if (result.bSuccess) {
+                addAsset(result.getPath());
+            }
+        }
+
+        if (ImGui::Button("Load Assets from Folder")) {
+            ofFileDialogResult result = ofSystemLoadDialog("Select a folder", true);
+            if (result.bSuccess) {
+                loadAssetsFromFolder(result.getPath());
+            }
+        }
+
+        const char* filterOptions[] = { "None", "Images", "Models" };
+        ImGui::Combo("Filter", &filterIndex, filterOptions, IM_ARRAYSIZE(filterOptions));
+
+        ImGui::Checkbox("Show Full Paths", &showFullPaths);
+
+        ImGui::EndChild();
+
+        ImGui::PopStyleVar(2);
+        ImGui::PopStyleColor(2);
+
+        ImGui::End();
     }
-
-    const char* filterOptions[] = { "None", "Images", "Models" };
-    ImGui::Combo("Filter", &filterIndex, filterOptions, IM_ARRAYSIZE(filterOptions));
-
-    ImGui::Checkbox("Show Full Paths", &showFullPaths);
-
-    ImGui::End();
 }
 
 void AssetBrowser::exit() {
@@ -75,55 +141,64 @@ void AssetBrowser::exit() {
 }
 
 void AssetBrowser::addAsset(const std::string& assetPath) {
-    if (fs::exists(assetPath) && std::find(assets.begin(), assets.end(), assetPath) == assets.end()) {
+    if (!fs::exists(assetPath)) {
+        return;
+    }
+
+    if (std::find(assets.begin(), assets.end(), assetPath) == assets.end()) {
         assets.push_back(assetPath);
     }
 }
 
 void AssetBrowser::removeAsset(const std::string& asset) {
-    assets.erase(std::remove(assets.begin(), assets.end(), asset), assets.end());
+    auto it = std::remove(assets.begin(), assets.end(), asset);
+    if (it != assets.end()) {
+        assets.erase(it, assets.end());
+    }
 }
 
 void AssetBrowser::selectAsset(const std::string& assetPath) {
-    auto it = std::find(assets.begin(), assets.end(), assetPath);
-    if (it != assets.end()) {
+    if (std::find(assets.begin(), assets.end(), assetPath) != assets.end()) {
         selectedAsset = assetPath;
     }
 }
 
 void AssetBrowser::loadAssetsFromFolder(const std::string& folderPath) {
-    if (fs::exists(folderPath) && fs::is_directory(folderPath)) {
-        for (const auto& entry : fs::directory_iterator(folderPath)) {
-            if (fs::is_regular_file(entry.path())) {
-                std::string assetPath = entry.path().string();
-                if (isImageAsset(assetPath) || isModelAsset(assetPath)) {
-                    addAsset(assetPath);
-                }
+    if (!fs::exists(folderPath) || !fs::is_directory(folderPath)) {
+        return;
+    }
+
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (fs::is_regular_file(entry.path())) {
+            std::string assetPath = entry.path().string();
+            if (isImageAsset(assetPath) || isModelAsset(assetPath)) {
+                addAsset(assetPath);
             }
         }
     }
 }
 
 bool AssetBrowser::isImageAsset(const std::string& asset) const {
-    std::string extension = fs::path(asset).extension().string();
-    return extension == ".png" || extension == ".jpg" || extension == ".jpeg";
+    static const std::set<std::string> imageExtensions = { ".png", ".jpg", ".jpeg" };
+    return imageExtensions.find(fs::path(asset).extension().string()) != imageExtensions.end();
 }
 
 bool AssetBrowser::isModelAsset(const std::string& asset) const {
-    std::string extension = fs::path(asset).extension().string();
-    return extension == ".obj" || extension == ".dae" || extension == ".fbx";
+    static const std::set<std::string> modelExtensions = { ".obj", ".dae", ".fbx" };
+    return modelExtensions.find(fs::path(asset).extension().string()) != modelExtensions.end();
 }
 
 std::vector<std::string> AssetBrowser::getFilteredAssets() const {
     std::vector<std::string> filteredAssets;
     std::copy_if(assets.begin(), assets.end(), std::back_inserter(filteredAssets), [&](const std::string& asset) {
-        if (filterIndex == 1) {
+        switch (filterIndex) {
+        case 1:
             return isImageAsset(asset);
-        }
-        else if (filterIndex == 2) {
+        case 2:
             return isModelAsset(asset);
+        default:
+            return true;
         }
-        return true;
         });
     return filteredAssets;
 }

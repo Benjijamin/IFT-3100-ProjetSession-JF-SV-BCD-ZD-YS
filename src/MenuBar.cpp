8586647b4652c5658c1849054bcfd70515e2c7c2
Yohan Sercia <yohansercia@outlook.com>
2025-03-06 17:10:12 -0500

    Réorganisation des implémentations dans l'interface générale afin d'avoir un code plus clair. En autre: >Encapsulation des méthodes dans la barre de menu >Implémentation d'une barre d'outil verticale pour plus tard

--- src/MenuBar.cpp
+++ src/MenuBar.cpp
@@ -1,82 +1,198 @@
 #include "MenuBar.h"
 
-void MenuBar::setup() {
-    screenCapture.setup();
-}
-
-void MenuBar::update() {
-    screenCapture.update();
-}
-
-void MenuBar::draw() {
-    screenCapture.draw();
-}
+void MenuBar::setup() { screenCapture.setup(); }
+void MenuBar::update() { screenCapture.update(); }
+void MenuBar::draw() { screenCapture.draw(); }
+void MenuBar::exit() {}
 
 void MenuBar::drawGui() {
-    if (ImGui::BeginMainMenuBar()) {
-        drawFileMenu();
-        drawSettingsMenu();
-        drawHelpMenu();
-        ImGui::EndMainMenuBar();
-    }
-    screenCapture.drawGui();
-}
+	if (ImGui::BeginMainMenuBar()) {
 
-void MenuBar::exit() {
-    screenCapture.exit();
+		drawFileMenu();
+		drawEditMenu();
+		drawRenderMenu();
+		drawWindowMenu();
+
+		drawHelpMenu();
+
+		ImGui::EndMainMenuBar();
+	}
+
+	drawToolBar();
+
+	dessinVectoriel.drawGui();
+	screenCapture.drawGui();
 }
 
 void MenuBar::drawFileMenu() {
-    if (ImGui::BeginMenu("File")) {
-        if (ImGui::MenuItem("New Drawing..")) {
-            if (onNewDrawing) onNewDrawing();
-        }
+	if (ImGui::BeginMenu("File")) {
+		drawProjectMenu();
 
-        if (ImGui::MenuItem("Capture Screenshot")) {
-            screenCapture.captureScreenshot();
-        }
+		ImGui::Separator();
 
-        if (ImGui::MenuItem("Exit")) {
-            ofExit();
-        }
-        ImGui::EndMenu();
-    }
+		drawDrawingMenu();
+
+		ImGui::Separator();
+
+		drawSettingsMenu();
+
+		ImGui::Separator();
+
+		drawExitMenu();
+
+		ImGui::EndMenu();
+	}
+}
+
+// MÉTHODES POUR 'ONGLET FILE'
+
+void MenuBar::drawProjectMenu() {
+	if (ImGui::BeginMenu("Project")) {
+		if (ImGui::MenuItem("New Project")) {}
+		if (ImGui::MenuItem("Open Project")) {
+			ofFileDialogResult result = ofSystemLoadDialog("Choisissez un fichier de projet ...");
+			if (result.bSuccess) { openProject(result.getPath()); }
+		}
+		if (ImGui::MenuItem("Save Project")) {
+			ofFileDialogResult result = ofSystemSaveDialog("monProjet.json", "Enregistrer sous ...");
+			if (result.bSuccess) { saveProject((result.getPath())); }
+		}
+
+		ImGui::EndMenu();
+	}
+}
+
+void MenuBar::drawDrawingMenu() {
+	if (ImGui::BeginMenu("Drawing")) {
+		if (ImGui::MenuItem("New Drawing..")) {
+			if (onNewDrawing) onNewDrawing();
+		}
+		if (ImGui::MenuItem("Export Drawing")) {
+			ofFileDialogResult result = ofSystemSaveDialog("monImage.png", "Enregistrer sous ...");
+			if (result.bSuccess) { imageEditor.save(result.getPath()); }
+		}
+		if (ImGui::MenuItem("Import Drawing")) {
+			ofFileDialogResult result = ofSystemLoadDialog("Choisissez un fichier image a importer ...");
+		}
+
+		ImGui::EndMenu();
+	}
 }
 
 void MenuBar::drawSettingsMenu() {
-    if (ImGui::BeginMenu("Settings")) {
-        if (ImGui::MenuItem("Classic Theme")) {
-            ImGui::StyleColorsClassic();
-            ofSetBackgroundColor(ofColor(60, 60, 60));
-        }
+	if (ImGui::BeginMenu("Settings")) {
+		if (ImGui::BeginMenu("Themes")) {
+			if (ImGui::MenuItem("Classic Theme")) {
+				ImGui::StyleColorsClassic();
+				ofSetBackgroundColor(ofColor(60, 60, 60));
+			}
 
-        if (ImGui::MenuItem("Light Theme")) {
-            ImGui::StyleColorsLight();
-            ofSetBackgroundColor(ofColor(230, 242, 255));
-        }
+			if (ImGui::MenuItem("Light Theme")) {
+				ImGui::StyleColorsLight();
+				ofSetBackgroundColor(ofColor(230, 242, 255));
+			}
 
-        if (ImGui::MenuItem("Dark Theme")) {
-            ImGui::StyleColorsDark();
-            ofSetBackgroundColor(ofColor(32, 32, 32));
-        }
+			if (ImGui::MenuItem("Dark Theme")) {
+				ImGui::StyleColorsDark();
+				ofSetBackgroundColor(ofColor(32, 32, 32));
+			}
 
-        if (ImGui::MenuItem("Fullscreen")) {
-            ofToggleFullscreen();
-        }
+			ImGui::EndMenu();
+		}
 
-        if (ImGui::MenuItem("Windowed")) {
-            ofSetFullscreen(false);
-        }
-
-        ImGui::EndMenu();
-    }
+		ImGui::EndMenu();
+	}
 }
 
+void MenuBar::drawExitMenu() {
+	if (ImGui::MenuItem("Exit")) { 
+		ofExit();
+	}
+}
+
+void MenuBar::saveProject(const std::string& filePath) {
+	ofFile file(filePath);
+	if (!file.exists()) {
+		ofLogError() << "Fichier non trouvé : " << filePath;
+		return;
+	}
+}
+
+void MenuBar::openProject(const std::string& filePath) {
+	ofFile file(filePath);
+	if (!file.exists()) {
+		ofLogError() << "Fichier non trouvé : " << filePath;
+		return;
+	}
+}
+
+
+// MÉTHODES POUR ONGLET 'EDIT'
+void MenuBar::drawEditMenu() {
+	if (ImGui::BeginMenu("Edit")) {
+		if (ImGui::MenuItem("Capture Screenshot")) { screenCapture.captureScreenshot(); }
+
+		ImGui::Separator();
+
+		if (ImGui::MenuItem("Undo", "Ctrl+Z")) {}
+		if (ImGui::MenuItem("Redo", "Ctrl+Y")) {}
+
+		ImGui::Separator();
+
+		if (ImGui::MenuItem("Cut", "Ctrl+X")) {}
+		if (ImGui::MenuItem("Copy", "Ctrl+C")) {}
+		if (ImGui::MenuItem("Paste", "Ctrl+V")) {}
+
+		ImGui::EndMenu();
+	}
+}
+
+
+// MÉTHODES POUR ONGLET 'RENDER'
+void MenuBar::drawRenderMenu() {
+	if (ImGui::BeginMenu("Render")) {
+		if (ImGui::MenuItem("Render Image")) {}
+		if (ImGui::MenuItem("Render Animation")) {}
+
+		ImGui::EndMenu();
+	}
+}
+
+
+// MÉTHODES POUR ONGLET 'WINDOW'
+void MenuBar::drawWindowMenu() {
+	if (ImGui::BeginMenu("Window")) {
+		if (ImGui::MenuItem("Toggle Fullscreen")) { ofToggleFullscreen(); }
+		if (ImGui::MenuItem("Windowed")) { ofSetFullscreen(false); }
+		if (ImGui::MenuItem("Split canva")) {}
+
+		ImGui::Separator();
+
+		if (ImGui::MenuItem("Reset Layout")) {}
+
+		ImGui::EndMenu();
+	}
+}
+
+// MÉTHODES POUR ONGLET 'HELP'
 void MenuBar::drawHelpMenu() {
-    if (ImGui::BeginMenu("Help")) {
-        if (ImGui::MenuItem("Documentation")) {
-            ofLaunchBrowser("https://openframeworks.cc/documentation/");
-        }
-        ImGui::EndMenu();
-    }
+	if (ImGui::BeginMenu("Help")) {
+		if (ImGui::MenuItem("Documentation")) {
+			ofLaunchBrowser("https://openframeworks.cc/documentation/");
+		}
+		ImGui::EndMenu();
+	}
+}
+
+
+// MÉTHODES POUR BARRE D'OUTILS VERTICALE
+void MenuBar::drawToolBar() {
+	ImGui::SetNextWindowPos(ImVec2(ofGetWidth() - 150, 20));
+	ImGui::SetNextWindowSize(ImVec2(50, 500));
+
+	ImGui::Begin("Toolbar", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+	
+	if (ImGui::Button("Dessin Vectoriel", ImVec2(100, 40))) { dessinVectoriel.begin(); }
+
+	ImGui::End();
 }
--- src/MenuBar.h
+++ src/MenuBar.h
@@ -1,24 +1,98 @@
 #pragma once
 
 #include "ofxImGui.h"
+#include "ofMain.h"
+
+#include "imageEditor.h"
 #include "ScreenCapture.h"
+#include "DessinVectoriel.h"
 
-
+/*
+* \brief Classe qui gère la barre de menu.
+*/
 class MenuBar {
+
 public:
-    void setup();
-    void update();
-    void draw();
-    void drawGui();
-    void exit();
+	void setup();
+	void update();
+	void draw();
+	void drawGui();
+	void exit();
 
     std::function<void()> onNewDrawing;
 
 private:
-    void drawFileMenu();
-    void drawSettingsMenu();
-    void drawHelpMenu();
+	// Objets issus d'autres classes pour liaison avec boutons de l'interface
+	ScreenCapture screenCapture;
+	ImageEditor imageEditor;
+	DessinVectoriel dessinVectoriel;
 
-    ScreenCapture screenCapture;
 
+
+	// ONGLET DES FICHIERS
+	//----------------------//
+	/*
+	* \brief Gère le bouton 'File' de l'interface.
+	* \brief La structure du bouton englobe des «sous-boutons» qui sont des méthodes appellées par drawFileMenu():
+	* \brief > void drawProjectMenu(); (Gestion des projets)
+	* \brief > void drawDrawingMenu();	(Gestion des dessins)
+	* \brief > drawSettingsMenu(); (Gestion des paramètres)
+	* \brief > drawExitMenu(); (Quitter l'application)
+	* \brief > saveProject(); et openProject(); (Sauvegarde globale du projet qui peut contenir plusieurs dessins (plus tard))
+	*/
+	void drawFileMenu();
+
+	void drawProjectMenu();
+	void drawDrawingMenu();
+	void drawSettingsMenu();
+	void drawExitMenu();
+	void saveProject(const std::string& filePath);
+	void openProject(const std::string& filePath);
+	//----------------------//
+	/*
+	* \brief Gère le bouton 'Edit' de l'interface.
+	* \brief La structure du bouton englobe des «sous-boutons» qui sont des méthodes appellées par drawEditMenu():
+	* \brief > 
+	*/
+	void drawEditMenu();
+
+	//----------------------//
+	/*
+	* \brief Gère le bouton 'Render' de l'interface.
+	* \brief La structure du bouton englobe des «sous-boutons» qui sont des méthodes appellées par drawRenderMenu():
+	* \brief > 
+	*/
+	void drawRenderMenu();
+
+	//----------------------//
+	/*
+	* \brief Gère le bouton 'Window' de l'interface.
+	* \brief La structure du bouton englobe des «sous-boutons» qui sont des méthodes appellées par drawWindowMenu():
+	* \brief > 
+	*/
+	void drawWindowMenu();
+
+	//----------------------//
+	/*
+	* \brief Gère le bouton 'Scene' de l'interface.
+	* \brief La structure du bouton englobe des «sous-boutons» qui sont des méthodes appellées par drawSceneMenu():
+	* \brief > 
+	*/
+	void drawSceneMenu();
+
+	//----------------------//
+	/*
+	* \brief Gère le bouton 'Help' dans l'interface.
+	* \brief La structure du bouton englobe des «sous-boutons» qui sont des méthodes appellées par drawHelpMenu():
+	* \brief > 
+	*/
+	void drawHelpMenu();
+
+	//----------------------//
+	/*
+	* \brief Gère le bouton 'Toolbar' à droite de l'interface.
+	* \brief La structure du bouton englobe des «sous-boutons» qui sont des méthodes appellées par drawToolBar():
+	* \brief > 
+	*/
+	void drawToolBar();
 };
